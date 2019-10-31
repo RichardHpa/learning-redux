d873097b6ca28e9194f6b2b1fd07294e133a327e
RichardHpa <richard.hpa@hotmail.com>
2019-10-31 18:11:14 +1300

    set up basic redux store on index for incrementing and decrementing a counter

--- src/index.js
+++ src/index.js
@@ -4,6 +4,41 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { createStore } from 'redux';
+
+//STORE -> a Global State
+
+//ACTION -> Describes what you want to do
+const increment = () => {
+    return {
+        type: 'INCREMENT'
+    }
+}
+
+const decrement = () => {
+    return{
+        type: 'DECREMENT'
+    }
+}
+
+//REDUCER
+const counter = (state = 0, action) => {
+    switch(action.type){
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+    }
+}
+
+let store = createStore(counter);
+
+//Show in console
+store.subscribe(() => console.log(store.getState()));
+
+//DISPATCH
+store.dispatch(increment());
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
